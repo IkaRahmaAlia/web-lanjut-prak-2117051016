71aefbd80fda25645270aa310a89c8d6a9a8038b
Ika rahma alia <ika418471@gmail.com>
2023-10-04 21:29:08 +0700

    Tugas 5 read data

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -4,11 +4,25 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\UserModel;
+use App\Models\KelasModel;
+
 class UserController extends BaseController
 {
+    public $userModel;
+    public $kelasModel;
+
+    public function __construct()
+    {
+        $this-> userModel = new UserModel();
+        $this->kelasModel = new KelasModel();
+    }
     public function index()
     {
-        //
+        $data = [
+            'title' => 'List User',
+            'users' => $this ->userModel->getUser(),
+        ];
+        return view ('list_user', $data);
     }
 
       public function profile($nama = "Ika Rahma Alia", $kelas = "CD", $npm = "2117051016")
@@ -24,26 +38,28 @@ class UserController extends BaseController
 
 
     public function create(){
-        $kelas = [
-            [
-                'id'=>1,
-                'nama_kelas'=>'A'
-            ],
-            [
-                'id'=>2,
-                'nama_kelas'=>'B'
-            ],
-            [
-                'id'=>3,
-                'nama_kelas'=>'C'
-            ],
-            [
-                'id'=>4,
-                'nama_kelas'=>'D'
-            ],
-        ];
+        // $kelas = [
+        //     [
+        //         'id'=>1,
+        //         'nama_kelas'=>'A'
+        //     ],
+        //     [
+        //         'id'=>2,
+        //         'nama_kelas'=>'B'
+        //     ],
+        //     [
+        //         'id'=>3,
+        //         'nama_kelas'=>'C'
+        //     ],
+        //     [
+        //         'id'=>4,
+        //         'nama_kelas'=>'D'
+        //     ],
+        // ];
 
+        $kelas = $this->kelasModel->getKelas();
         $data=[
+            'title' => 'Create User',
             'kelas' =>$kelas,
             //vidio
             'validation' => \Config\Services::validation()
@@ -82,12 +98,14 @@ class UserController extends BaseController
 
         ]);
 
-        $data = [
-            'nama' => $this -> request->getVar('nama'),
-            'kelas' => $this -> request->getVar('kelas'),
-            'npm' => $this -> request->getVar('npm'),
+        // $data = [
+        //     'nama' => $this -> request->getVar('nama'),
+        //     'kelas' => $this -> request->getVar('kelas'),
+        //     'npm' => $this -> request->getVar('npm'),
             
-        ];
-        return view('profile', $data);
+        // ];
+        // return view('profile', $data);
+
+        return redirect()->to('/user');
     }
 }
--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form</title>
-    <link rel="stylesheet" href="<?=base_url("assets/css/style.css")?>">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-</head>
-<body>
+<?= $this -> extend('layouts/app') ?>
+<?= $this -> section('content') ?>
+
     <br>
     <div class="container">
         <form action="<?=base_url('user/store')?>" method="post">
@@ -61,30 +54,5 @@
             </div>
         </form>
     </div>
-    <table>
-        <!-- <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td><input type="text" name="nama"></td>
-        </tr> -->
-        <!-- <tr>
-            <td>NPM</td>
-            <td>:</td>
-            <td><input type="text" name="npm"></td>
-        </tr>
-         <tr>
-            <td>Kelas</td>
-            <td>:</td>
-            <td><input type="text" name="kelas"></td>
-        </tr>
-         <tr>
-            <td>Alamat</td>
-            <td>:</td>
-            <td><input type="text" name="alamat"></td>
-        </tr> 
-        <tr>
-            <td><input type="submit" value="Simpan"></td>
-        </tr> -->
-    </table>
-</body>
-</html>
+
+<?=$this->endSection() ?>
